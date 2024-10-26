d94c85452e698e91fc30e651591097c61fd73d42
TaiDevFe <lehoanganhtai1115@gmail.com>
2024-10-26 11:27:07 +0700

    edit text

--- src/pages/ChatDetail.jsx
+++ src/pages/ChatDetail.jsx
@@ -33,7 +33,7 @@ const ChatDetail = () => {
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+            if (sidebarref.current && !sidebarref.current.contains(event.target)) {
                 setMenuToggle(false); // Ẩn sidebar khi nhấn bên ngoài
             }
         };
@@ -71,7 +71,7 @@ const ChatDetail = () => {
                 <button  onClick={() => setMenuToggle(!menuToggle)}>
                     <img src={IconMenu} alt='menu icon' className='w-10 h-10 xl:hidden'></img>
                 </button>
-                <h1 className='text-xl uppercase font-bold'>tàiai</h1>
+                <h1 className='text-xl uppercase font-bold'>tài-ai</h1>
             </div>
             {
                 menuToggle && (
@@ -103,17 +103,17 @@ const ChatDetail = () => {
                     <div className='flex flex-col space-y-5'>
                         <div className='space-y-1'>
                             <h2 className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-[30px] inline-block text-transparent bg-clip-text'>Xin chào</h2>
-                            <p className='text-3xl font-bold'>Hãy tạ</p>
+                            <p className='text-3xl font-bold'>Hãy tạo Cuộc trò chuyện mới để bắt đầu !</p>
                         </div>
                         <div className='flex items-center space-x-3'>
                             <div className='w-[200px] h-[200px] bg-primaryBg-sideBar flex items-center justify-center rounded-lg'>
-                                <p>Lên kế hoạch bữa ăn</p>
+                                <p>Tìm hiểu về ngôn ngữ</p>
                             </div>
                             <div className='w-[200px] h-[200px] bg-primaryBg-sideBar flex items-center justify-center rounded-lg'>
                                 <p>Cụm từ ngôn ngữ mới</p>
                             </div>
                             <div className='w-[200px] h-[200px] bg-primaryBg-sideBar flex items-center justify-center rounded-lg'>
-                                <p>Cách viết CV Front End</p>
+                                <p>Front End</p>
                             </div>
                             <div className='w-[200px] h-[200px] bg-primaryBg-sideBar flex flex-col items-center justify-center rounded-lg'>
                                 <p>Tạo hình ảnh AI</p>
